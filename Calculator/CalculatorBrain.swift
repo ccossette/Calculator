17376bc4318036f0dff85976b0cb8c81eb1c1675
Carlo Cossette <carlocossette@gmail.com>
2015-06-17 16:30:13 -0700

    Added functionality

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,108 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Carlo Cossette on 2015-05-18.
+//  Copyright (c) 2015 Carlo Cossette. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain{
+    
+    private enum Op: Printable {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+            
+        }
+    }
+    
+    private var opStack = [Op]()
+    
+    private var knownOps = [String: Op]()
+    
+    init(){
+        knownOps["✖️"] = Op.BinaryOperation("✖️", *)
+        knownOps["➗"] = Op.BinaryOperation("➗"){$1 / $0}
+        knownOps["➕"] = Op.BinaryOperation("➕"){$0 + $1}
+        knownOps["➖"] = Op.BinaryOperation("➖"){$1 - $0}
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+    }
+    
+    var program: AnyObject { //Guaranteed to be a PropertyList
+        get{
+            return opStack.map { $0.description }
+        }
+        set{
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op = knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    } else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(.Operand(operand))
+                    }
+                }
+                opStack = newOpStack
+            }
+        }
+    }
+    
+    private func evaluate (ops: [Op]) -> (result:Double?, remainingOps:[Op])
+    {
+        if !ops.isEmpty{
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op{
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result
+                {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        println("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOperand(operand:Double) -> Double?{
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol:String) -> Double? {
+        if let operation = knownOps[symbol]{
+        opStack.append(operation)
+        }
+        return evaluate()
+    }
+}
